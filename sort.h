ac258360bfd8e930bcff20b2e3c64e7ed5194141
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-01-18 18:47:01 -0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -24,4 +24,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sorty(int *array, int *p_array, size_t size, size_t p_size);
+void quick_sort(int *array, size_t size);
 #endif
