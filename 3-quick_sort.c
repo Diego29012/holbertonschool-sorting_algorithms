7806125d288abfbb3629d862342607f96316930c
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-01-21 04:55:39 +0000

    fixed bug style

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,12 +1,12 @@
 #include "sort.h"
 
 /**
-* quick_sorty - function that sorts an array
-* @array: array to sort
-* @p_array: original array
-* @size: size of the array
-* @p_size: original size
-**/
+ * quick_sorty - function that sorts an array
+ * @array: array to sort
+ * @p_array: original array
+ * @size: size of the array
+ * @p_size: original size
+*/
 
 void quick_sorty(int *array, int *p_array, size_t size, size_t p_size)
 {
@@ -16,20 +16,20 @@ void quick_sorty(int *array, int *p_array, size_t size, size_t p_size)
 	int temp_num;
 
 	if (!array || size == 0)
-	return;
+		return;
 
 	for (index = 0; index < end; index++)
 	{
-	if (array[end] > array[index])
-	{
-	temp_num = array[index];
-	array[index] = array[target];
-	array[target] = temp_num;
-	target++;
+		if (array[end] > array[index])
+		{
+			temp_num = array[index];
+			array[index] = array[target];
+			array[target] = temp_num;
+			target++;
 
-	if (index >= target)
-	print_array(p_array, p_size);
-	}
+		if (index >= target)
+			print_array(p_array, p_size);
+		}
 	}
 
 	temp_num = array[end];
@@ -37,19 +37,19 @@ void quick_sorty(int *array, int *p_array, size_t size, size_t p_size)
 	array[target] = temp_num;
 
 	if (end != target && array[index] != array[target])
-	print_array(p_array, p_size);
+		print_array(p_array, p_size);
 	quick_sorty(&array[0], p_array, target, p_size);
 	quick_sorty(&array[target + 1], p_array, end - target, p_size);
 }
 
 /**
-* quick_sort - function that sorts an array
-* @array: array to sort
-* @size: array size
-**/
+ * quick_sort - function that sorts an array
+ * @array: array to sort
+ * @size: array size
+*/
 void quick_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
-	return;
+		return;
 	quick_sorty(array, array, size, size);
 }
